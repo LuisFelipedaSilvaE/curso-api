1f1447ee04d5744d7d82836e635a444e7c458555
LuisFelipedaSilvaE <luisfelipe070123@gmail.com>
2025-06-09 16:23:39 -0300

    teste para endpoint update da classe user resource implementado

--- src/test/java/org/example/api/resources/UserResourceTest.java
+++ src/test/java/org/example/api/resources/UserResourceTest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,7 +21,6 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 class UserResourceTest {
 
-    public static final int INDEX = 0;
     @InjectMocks
     private UserResource resource;
 
@@ -36,6 +34,7 @@ class UserResourceTest {
     public static final String NAME = "Jonathan";
     public static final String EMAIL = "jona@gmail.com";
     public static final String PASSWORD = "J0J0";
+    public static final int INDEX = 0;
 
     private User user;
     private UserDTO userDTO;
@@ -97,7 +96,21 @@ class UserResourceTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(service.update(userDTO)).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.update(ID, userDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
     }
 
     @Test
